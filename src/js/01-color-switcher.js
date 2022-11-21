ccea668a38645011a056f7e02543249064cb4ab6
InnaLyksa <in.lyksa@gmail.com>
2022-11-21 10:30:23 +0200

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+startBtn.addEventListener('click', onClickStartBtn);
+stopBtn.addEventListener('click', onClickStopBtn);
+
+function onClickStartBtn() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+}
+
+function onClickStopBtn() {
+  clearInterval(timerId);
+
+  stopBtn.setAttribute('disabled', true);
+  startBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
